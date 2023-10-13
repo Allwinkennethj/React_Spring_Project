a132170c33afb0d469404541e06deab7db16b849
Allwin Kenneth J <110586794+Allwinkennethj@users.noreply.github.com>
2023-10-13 10:42:06 +0530

    Update LoginStreakController.java

--- src/main/java/com/example/backend/Controller/LoginStreakController.java
+++ src/main/java/com/example/backend/Controller/LoginStreakController.java
@@ -9,12 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 
 public class LoginStreakController {
-    @Autowired
-    private LoginStreakRepo loginStreakRepo;
-    @GetMapping("/GetLoginStreakforParticularUser")
-    public int GetLoginStreakforParticularUser(@PathVariable Long userid)
-    {
-        return loginStreakRepo.getLoginStreak(userid);
-    }
+    //@Autowired
+    //private LoginStreakRepo loginStreakRepo;
+    //@GetMapping("/GetLoginStreakforParticularUser")
+    //public int GetLoginStreakforParticularUser(@PathVariable Long userid)
+    //{
+    //    return loginStreakRepo.getLoginStreak(userid);
+    //}
 
 }
