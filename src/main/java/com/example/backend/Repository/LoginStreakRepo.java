d1dd21b6e1d95a7b749f4b35fcd51488aae4ad68
AllwinDivum <kenneth.allwin@divum.in>
2023-10-13 11:29:01 +0530

    add

--- src/main/java/com/example/backend/Repository/LoginStreakRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.backend.Repository;
-
-import com.example.backend.Entity.LoginStreak;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LoginStreakRepo extends JpaRepository<LoginStreak,Long> {
-    // int getLoginStreak(Long userid);
-}
--- src/main/java/com/example/backend/Service/LoginStreakService.java
+++
@@ -1,5 +0,0 @@
-package com.example.backend.Service;
-
-public interface LoginStreakService {
-    // public int getLoginStreak(Long userid);
-}
