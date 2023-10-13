f0a00310931a28be9d27cfdfca01dc5e6c48d4ba
Allwin Kenneth J <110586794+Allwinkennethj@users.noreply.github.com>
2023-10-13 10:43:38 +0530

    Update LoginStreakImplementation.java

--- src/main/java/com/example/backend/Service/LoginStreakImplementation.java
+++ src/main/java/com/example/backend/Service/LoginStreakImplementation.java
@@ -12,51 +12,51 @@ import java.util.Optional;
 
 @Service
 public class LoginStreakImplementation implements LoginStreakService{
-    @Autowired
-    private LoginStreakService loginStreakService;
-    @Autowired
-    private LoginStreakRepo loginStreakRepo;
+    // @Autowired
+    // private LoginStreakService loginStreakService;
+    // @Autowired
+    // private LoginStreakRepo loginStreakRepo;
 
-    @Autowired
-    private UserRepository userRepository;
+    // @Autowired
+    // private UserRepository userRepository;
 
-    @Override
-    public int getLoginStreak(Long userid) {
-        Optional<LoginStreak> streak= loginStreakRepo.findById(userid);
-        if(streak.isPresent())
-        {
-         LoginStreak loginStreak=streak.get();
-         LocalDate lastLoginDate=loginStreak.getLastLoginDate();
-         int currentStreak=loginStreak.getStreakCount();
-         LocalDate currentDate=LocalDate.now();
-         if(lastLoginDate==null)
-         {
-             loginStreak.setStreakCount(1);
-             loginStreak.setLastLoginDate(currentDate);
-         }
-         else if(lastLoginDate.isEqual(currentDate.minusDays(1))){
-             loginStreak.setLastLoginDate(currentDate);
-             loginStreak.setStreakCount(currentStreak+1);
+    // @Override
+    // public int getLoginStreak(Long userid) {
+    //     Optional<LoginStreak> streak= loginStreakRepo.findById(userid);
+    //     if(streak.isPresent())
+    //     {
+    //      LoginStreak loginStreak=streak.get();
+    //      LocalDate lastLoginDate=loginStreak.getLastLoginDate();
+    //      int currentStreak=loginStreak.getStreakCount();
+    //      LocalDate currentDate=LocalDate.now();
+    //      if(lastLoginDate==null)
+    //      {
+    //          loginStreak.setStreakCount(1);
+    //          loginStreak.setLastLoginDate(currentDate);
+    //      }
+    //      else if(lastLoginDate.isEqual(currentDate.minusDays(1))){
+    //          loginStreak.setLastLoginDate(currentDate);
+    //          loginStreak.setStreakCount(currentStreak+1);
 
-         }else {
-             loginStreak.setStreakCount(1);
-             loginStreak.setLastLoginDate(currentDate);
-         }
-           loginStreakRepo.save(loginStreak);
-         return loginStreak.getStreakCount();
-        }
-        else {
-            LoginStreak newlogin=new LoginStreak();
-            newlogin.setLastLoginDate(LocalDate.now());
-            newlogin.setStreakCount(1);
-            Optional<Userinfo> userinfo = userRepository.findById(userid);
-            if (userinfo.isEmpty()) {
-                throw new RuntimeException("user not found");
-            }
-            newlogin.setUser(userinfo.get());
-            loginStreakRepo.save(newlogin);
-            return newlogin.getStreakCount();
-        }
+    //      }else {
+    //          loginStreak.setStreakCount(1);
+    //          loginStreak.setLastLoginDate(currentDate);
+    //      }
+    //        loginStreakRepo.save(loginStreak);
+    //      return loginStreak.getStreakCount();
+    //     }
+    //     else {
+    //         LoginStreak newlogin=new LoginStreak();
+    //         newlogin.setLastLoginDate(LocalDate.now());
+    //         newlogin.setStreakCount(1);
+    //         Optional<Userinfo> userinfo = userRepository.findById(userid);
+    //         if (userinfo.isEmpty()) {
+    //             throw new RuntimeException("user not found");
+    //         }
+    //         newlogin.setUser(userinfo.get());
+    //         loginStreakRepo.save(newlogin);
+    //         return newlogin.getStreakCount();
+    //     }
 
-    }
+    // }
 }
