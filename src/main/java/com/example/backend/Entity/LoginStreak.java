6ccb38ce812aa91182c5f97669d34be58a005830
Allwin Kenneth J <110586794+Allwinkennethj@users.noreply.github.com>
2023-10-13 10:42:49 +0530

    Update LoginStreak.java

--- src/main/java/com/example/backend/Entity/LoginStreak.java
+++ src/main/java/com/example/backend/Entity/LoginStreak.java
@@ -6,60 +6,60 @@ import java.time.LocalDate;
 
 @Entity
 public class LoginStreak {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private Userinfo user;
+//     @ManyToOne
+//     @JoinColumn(name = "user_id")
+//     private Userinfo user;
 
-    private int streakCount;
-    private LocalDate lastLoginDate;
+//     private int streakCount;
+//     private LocalDate lastLoginDate;
 
 
-    public LoginStreak() {
+//     public LoginStreak() {
 
-    }
+//     }
 
-    public LoginStreak(Userinfo user, int streakCount, LocalDate lastLoginDate) {
-        this.user = user;
-        this.streakCount = streakCount;
-        this.lastLoginDate = lastLoginDate;
-    }
+//     public LoginStreak(Userinfo user, int streakCount, LocalDate lastLoginDate) {
+//         this.user = user;
+//         this.streakCount = streakCount;
+//         this.lastLoginDate = lastLoginDate;
+//     }
 
-    public Long getId() {
-        return id;
-    }
+//     public Long getId() {
+//         return id;
+//     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+//     public void setId(Long id) {
+//         this.id = id;
+//     }
 
-    public Userinfo getUser() {
-        return user;
-    }
+//     public Userinfo getUser() {
+//         return user;
+//     }
 
-    public void setUser(Userinfo user) {
-        this.user = user;
-    }
+//     public void setUser(Userinfo user) {
+//         this.user = user;
+//     }
 
-    public int getStreakCount() {
-        return streakCount;
-    }
+//     public int getStreakCount() {
+//         return streakCount;
+//     }
 
-    public int setStreakCount(int streakCount) {
-        this.streakCount = streakCount;
-        return streakCount;
-    }
+//     public int setStreakCount(int streakCount) {
+//         this.streakCount = streakCount;
+//         return streakCount;
+//     }
 
-    public LocalDate getLastLoginDate() {
-        return lastLoginDate;
-    }
+//     public LocalDate getLastLoginDate() {
+//         return lastLoginDate;
+//     }
 
-    public void setLastLoginDate(LocalDate lastLoginDate) {
-        this.lastLoginDate = lastLoginDate;
-    }
+//     public void setLastLoginDate(LocalDate lastLoginDate) {
+//         this.lastLoginDate = lastLoginDate;
+//     }
 
 
 }
